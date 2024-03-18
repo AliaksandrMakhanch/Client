2e86a696962b6bf96d79ae6440f8ccd9f8520e8f
EPAM\Aliaksandr_Makhnach <aliaksandr_makhnach@epam.com>
2024-03-18 17:14:33 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientApp
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            await ProcessRequest("/MyName");
+            await ProcessRequest("/Information");
+            await ProcessRequest("/Success");
+            await ProcessRequest("/Redirection");
+            await ProcessRequest("/ClientError");
+            await ProcessRequest("/ServerError");
+            await ProcessRequest("/MyNameByHeader");
+        }
+
+        private static async Task ProcessRequest(string requestUri)
+        {
+            HttpResponseMessage response = await client.GetAsync("http://localhost:8888" + requestUri);
+
+            Console.WriteLine($"Request: {requestUri}, Status Code: {response.StatusCode}");
+
+            if (response.Headers.Contains("X-MyName"))
+            {
+                Console.WriteLine("X-MyName: " + response.Headers.GetValues("X-MyName").FirstOrDefault());
+            }
+            else
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response Body: " + responseBody);
+            }
+        }
+    }
+}
